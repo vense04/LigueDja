7f21b5bf88fb58cbe708b15fb7c8137a4132a323
vense04 <vense04@gmail.com>
2014-07-07 09:25:39 -0300

    teste

--- src/servico/ClienteServico.java
+++ src/servico/ClienteServico.java
@@ -9,17 +9,20 @@ import com.sun.org.apache.regexp.internal.recompile;
 
 import dao.EnderecoDao;
 import dao.UsuarioDao;
+import dao.UsuarioPermissaoDao;
 import dominio.Usuario;
+import dominio.UsuarioPermissao;
 
 public class ClienteServico {
 	
 	@Inject
 	EntityManager em;
-	
+
 	@Inject
 	private UsuarioDao usuarioDao;
 	
-	
+	@Inject
+	private UsuarioPermissaoDao usuarioPerUsuarioDao;
 	
 	public List<Usuario> listarClientes(){
 		return usuarioDao.buscaTodos();
@@ -34,13 +37,22 @@ public class ClienteServico {
 		try{
 			em.getTransaction().begin();
 			usuarioDao.inserir(us);
+			
 			em.getTransaction().commit();
+			
 		} catch(Throwable e){
 			if (em.getTransaction().isActive())
 				em.getTransaction().rollback();
 			e.printStackTrace();
 			throw new RuntimeException(" Erro ao Inserir Cadastro");
 		}
+	}
+	
+	public void inserirPermissao(UsuarioPermissao usuarioPermissao) {
+		
+		em.getTransaction().begin();
+		usuarioPerUsuarioDao.inserirPermissao(usuarioPermissao);
+		em.getTransaction().commit();
 	}
 	
 	public Integer getLastId() {
@@ -60,6 +72,7 @@ public class ClienteServico {
 			throw new RuntimeException(" Erro ao Atualizar Cadastro Cliente");
 		}
 	}
+	
 	
 
 }
